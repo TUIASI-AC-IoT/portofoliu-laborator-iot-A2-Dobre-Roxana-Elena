79707b6d4e08806883a1bf066d9b1721c316499a
Roxana-Elena Dobre <79217476+A2-Dobre-Roxana-Elena@users.noreply.github.com>
2025-02-26 19:51:11 +0200

    Update udp_sender.py

--- Lab2/udp_sender.py
+++ Lab2/udp_sender.py
@@ -2,19 +2,30 @@ import socket
 import time
 
 # Completati cu adresa IP a platformei ESP32
-PEER_IP = "192.168.89.30"
+PEER_IP = "192.168.89.50"
 PEER_PORT = 10001
 
 MESSAGE = b"Salut!"
+LED_OFF = b"GPIO4=0"
+LED_ON  = b"GPIO4=1"
+
 i = 0
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while 1:
+    # Modify the existing application to enable LED control through UDP messages. The
+    # LED is connected to GPIO4, and its state will be toggled based on received datagrams
     try:
-        TO_SEND = MESSAGE + bytes(str(i),"ascii")
-        sock.sendto(TO_SEND, (PEER_IP, PEER_PORT))
+        if i % 2:
+            TO_SEND = LED_ON
+            sock.sendto(TO_SEND, (PEER_IP, PEER_PORT))
+        else:
+            TO_SEND = LED_OFF
+            sock.sendto(TO_SEND, (PEER_IP, PEER_PORT))
+
         print("Am trimis mesajul: ", TO_SEND)
         i = i + 1
         time.sleep(1)
     except KeyboardInterrupt:
         break
+        
